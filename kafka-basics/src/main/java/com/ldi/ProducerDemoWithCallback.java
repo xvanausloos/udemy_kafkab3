83a0fd778b46c41963702ab1d5106c5c8ab50225
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2023-10-16 10:29:03 +0200

    module 46 Kafka Java producer callback

--- kafka-basics/src/main/java/com/ldi/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/com/ldi/ProducerDemoWithCallback.java
@@ -1,4 +1,4 @@
-package com.ldi;
+ package com.ldi;
 
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 import java.util.Properties;
 
 /*
-Module 46 Java producer callback
+Module 46 Java producer callback + module 47 with Keys
 Prerequisite: use a topic with several partitions
  kafka-topics.sh --bootstrap-server localhost:9092 --alter --topic demo_java --partitions 3
  */
@@ -30,14 +30,18 @@ public class ProducerDemoWithCallback {
         //create the producer
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
-        // properties.setProperty("batch.size", "400"); keep default setting in prod
+        properties.setProperty("batch.size", "400"); // just fior showing sticky partition do not keep default setting in prod
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-        for (int j = 0; j < 10; j++) {
-            for (int i = 0; i < 30; i++) {
+        for (int j = 0; j <2; j++) {
+            for (int i = 0; i < 10; i++) {
+                String topic = "demo_java";
+                String key = "id_" + i;
+                String value = "hello world " + i;
+
                 //create a producer record
-                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello_world" + i);
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, key, value);
 
                 //send data
                 producer.send(producerRecord, new Callback() {
@@ -45,21 +49,15 @@ public class ProducerDemoWithCallback {
                     public void onCompletion(RecordMetadata metadata, Exception e) {
                         //executes every time a record successfully sent or an exception is thrown
                         if (e == null) {
-                            log.info("received new metadata \n" +
-                                    "Topic: " + metadata.topic() + "\n" +
-                                    "Partition: " + metadata.partition() + "\n" +
-                                    "Offset: " + metadata.offset() + "\n" +
-                                    "Timestamp: " + metadata.timestamp() + "\n"
-                            );
+                            log.info("Key : " + key + " - Partition: " + metadata.partition() + "\n"  );
                         } else {
                             log.error("Error while producing", e);
                         }
                     }
                 });
-            }
             Thread.sleep(500);
+            }
         }
-
 
         //flush and close the producer: tell the producer to send all data and block until done --synchronous
         producer.flush(); //useless if you use producer.close() flush is doing by close()
